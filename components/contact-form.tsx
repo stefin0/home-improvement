1b53d4b3adda97837c81e2d60a2612e39f9d6070
stefin <stefinracho@gmail.com>
2025-08-12 01:57:58 -0700

    continue free-quote page

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -3,9 +3,11 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { isValidPhoneNumber } from "react-phone-number-input";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -14,18 +16,28 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { PhoneInput } from "@/components/ui/phone-input";
+import { services } from "@/config/services";
+import { Checkbox } from "@/components/ui/checkbox";
 
 const formSchema = z.object({
   name: z
     .string()
     .min(2, { message: "Name must be at least 2 characters." })
-    .max(50),
+    .max(25, { message: "Name must be less than 25 characters." }),
+  email: z.email(),
+  phone: z
+    .string()
+    .refine(isValidPhoneNumber, { message: "Invalid phone number" }),
+  services: z.array(z.string()).refine((value) => value.some((item) => item), {
+    message: "Please select at least one service.",
+  }),
 });
 
 export default function ContactForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: { name: "" },
+    defaultValues: { name: "", email: "", phone: "", services: [] },
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
@@ -37,12 +49,13 @@ export default function ContactForm() {
       <CardContent>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+            {/* Name */}
             <FormField
               control={form.control}
               name="name"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Name</FormLabel>
+                  <FormLabel className="text-base">Name</FormLabel>
                   <FormControl>
                     <Input {...field} />
                   </FormControl>
@@ -50,6 +63,90 @@ export default function ContactForm() {
                 </FormItem>
               )}
             />
+
+            {/* Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Email</FormLabel>
+                  <FormControl>
+                    <Input {...field} type="email" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Phone */}
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Phone Number</FormLabel>
+                  <FormControl>
+                    <PhoneInput defaultCountry="US" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Services */}
+            <FormField
+              control={form.control}
+              name="services"
+              render={() => (
+                <FormItem>
+                  <div className="mb-2">
+                    <FormLabel className="text-base">Services</FormLabel>
+                    <FormDescription>
+                      Select the services you are interested in.
+                    </FormDescription>
+                  </div>
+                  <div className="grid gap-6">
+                    {services.map((item) => (
+                      <FormField
+                        key={item.href}
+                        control={form.control}
+                        name="services"
+                        render={({ field }) => {
+                          return (
+                            <FormItem key={item.href} className="flex">
+                              <FormControl>
+                                <Checkbox
+                                  checked={field.value?.includes(item.title)}
+                                  onCheckedChange={(checked) => {
+                                    return checked
+                                      ? field.onChange([
+                                          ...field.value,
+                                          item.title,
+                                        ])
+                                      : field.onChange(
+                                          field.value?.filter(
+                                            (value) => value !== item.title,
+                                          ),
+                                        );
+                                  }}
+                                />
+                              </FormControl>
+                              <FormLabel className="font-normal">
+                                {item.title}
+                              </FormLabel>
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    ))}
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* Submit Button */}
             <Button type="submit">Submit</Button>
           </form>
         </Form>
